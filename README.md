35a7499fdfe100946a2cf4247bd55ce37bfccf09
王思远 <1731748271@qq.com>
2023-12-11 01:30:48 +0800

    计次服务设计

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# count-service
-提供通用计次能力，支持活动、奖品、任务等
+# count-service 计次服务
+提供通用的计次能力，支持活动、奖品、任务等。
+
+
+## 一、目标
+计次服务支持多维度（用户id/手机号/身份证号/设备号等）、多周期（相对/绝对周期、2天3次/每周1次等）的计次能力。
+
+以活动领取限制举例，原型设计如下：
+![](resource/计次-原型设计图.jpg)
+
+## 二、整体架构
+![](resource/计次-服务架构图.jpg)
+
+## 三、存储设计
+count_config 计次配置表：主要存储计次id和计次规则；
+count_record 计次流水表：用于存储计次流水，和做幂等判断；
+count_cycle 计次周期表：是用户在某计次下流水的汇总，用于计次查询和准入的判断；
+
+其中，count_record表和count_cycle表以dimension_id字段进行分库分表，来满足大数据量的存储。
+![](resource/计次-数据模型设计v2.jpg)
+Refer：https://www.processon.com/v/6571657440125a40fd13310b
+
+
+## 四、流程设计
+计次服务包括的核心功能：创建计次配置、计次查询、计次准入和计次逆向。
+### 1. 创建计次配置
+![](resource/计次-创建计次配置流程.jpg)
+### 2. 计次查询
+![](resource/计次-计次查询流程.jpg)
+### 3. 计次准入
+![](resource/计次-计次准入流程.jpg)
+### 4. 计次逆向
+![](resource/计次-计次逆向流程.jpg)
+
+
+## 五、接口定义
+```    
+B侧接口：CountConfigApi
+1. 生成计次配置id
+    Result<String> generateCountId(String countType);
+2. 创建计次配置
+    Result<String> createCountConfig(CountConfigParam param);
+
+C侧接口：CountServiceApi 
+1. 查询计次配置
+    Result<CountConfigDto> queryCountConfig(String countId);
+2. 计次查询/校验
+    Result<CountCheckDto> checkCount(CountCheckParam param);
+3. 计次准入
+    Result<CountResultDto> insertCount(CountInsertParam param);
+4. 计次逆向
+    Result<CountResultDto> rollbackCount(CountRollbackParam param);
+5. 用户计次纪录查询
+    Result<CountUserRecordDto> queryUserCountRecord(CountUserRecordParam param);
+```
+
+## 六、压力测试
+todo
+
+## 七、快速开始
+todo
+
---
+++ resource/计次-mysql.sql
@@ -0,0 +1,58 @@
+CREATE TABLE `count_config` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
+  `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '修改时间',
+  `creator` varchar(16)  NOT NULL DEFAULT '' COMMENT '创建人',
+  `updater` varchar(16)  NOT NULL DEFAULT '' COMMENT '修改人',
+  `count_id` varchar(64)  NOT NULL DEFAULT '' COMMENT '计次id',
+  `count_type` varchar(16)  NOT NULL DEFAULT '' COMMENT '计次类型（活动、奖品、任务等）',
+  `count_name` varchar(256)  NOT NULL DEFAULT '' COMMENT '计次名称',
+  `dimension_type` varchar(32)  NOT NULL DEFAULT '' COMMENT '计次维度（用户id、手机号、身份证、设备号等）',
+  `state` varchar(16)  NOT NULL DEFAULT '' COMMENT '计次状态',
+  `start_time` datetime(3) NOT NULL COMMENT '计次周期',
+  `end_time` datetime(3) NOT NULL COMMENT '计次周期',
+  `count_rule` varchar(512)  NOT NULL DEFAULT '' COMMENT '计次规则（2天3次，相对/绝对周期）',
+  `extend_info` varchar(1024)  NOT NULL DEFAULT '' COMMENT '扩展信息',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_count_id` (`count_id`),
+  KEY `idx_count_type` (`count_type`),
+  KEY `idx_create_time` (`create_time`)
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '计次配置表'
+
+
+CREATE TABLE `count_record` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
+  `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '修改时间',
+  `request_no` varchar(128)  NOT NULL DEFAULT '' COMMENT '幂等号（uuid、订单号等）',
+  `dimension_type` varchar(32)  NOT NULL DEFAULT '' COMMENT '计次维度（用户id、手机号、身份证、设备号等）',
+  `dimension_id` varchar(128)  NOT NULL DEFAULT '' COMMENT '计次维度值（分库分表字段）',
+  `count_id` varchar(64)  NOT NULL DEFAULT '' COMMENT '计次id',
+  `count_type` varchar(16)  NOT NULL DEFAULT '' COMMENT '计次类型（活动、奖品、任务等）',
+  `count_num` int(10) NOT NULL DEFAULT '0' COMMENT '计次数量',
+  `occur_time` datetime(3) NOT NULL COMMENT '计次时间',
+  `extend_info` varchar(1024)  NOT NULL DEFAULT '' COMMENT '扩展信息（计次来源：活动id、奖品id、任务id等）',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_request_no` (`request_no`),
+  KEY `idx_dimension_id_count_id` (`dimension_id`, `count_id`),
+  KEY `idx_create_time` (`create_time`)
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '计次流水表'
+
+
+CREATE TABLE `count_cycle` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增id',
+  `create_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
+  `update_time` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '修改时间',
+  `dimension_type` varchar(32)  NOT NULL DEFAULT '' COMMENT '计次维度（用户id、手机号、身份证、设备号等）',
+  `dimension_id` varchar(128)  NOT NULL DEFAULT '' COMMENT '计次维度值（分库分表字段）',
+  `count_id` varchar(64)  NOT NULL DEFAULT '' COMMENT '计次id',
+  `count_type` varchar(16)  NOT NULL DEFAULT '' COMMENT '计次类型（活动、奖品、任务等）',
+  `count_cycle_info` varchar(512)  NOT NULL DEFAULT '' COMMENT '计次周期信息',
+  `extend_info` varchar(1024)  NOT NULL DEFAULT '' COMMENT '扩展信息',
+  PRIMARY KEY (`id`),
+  KEY `idx_dimension_id_count_id` (`dimension_id`, `count_id`),
+  KEY `idx_create_time` (`create_time`)
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin COMMENT = '计次周期表'
+
+
+
