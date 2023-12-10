5f103e0a755d65cbd4783b4ced81502e7310859c
王思远 <1731748271@qq.com>
2023-12-11 02:09:23 +0800

    项目添加模块api和app

---
+++ count-app/src/main/java/com/balloon/CountApp.java
@@ -0,0 +1,16 @@
+package com.balloon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王思远
+ * @date 2023-12-11 01:59
+ */
+@SpringBootApplication
+public class CountApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountApp.class, args);
+    }
+}
---
+++ count-app/src/main/java/com/balloon/top/TestController.java
@@ -0,0 +1,8 @@
+package com.balloon.top;
+
+/**
+ * @author wangshiwei01
+ * @date 2023-12-11 01:55
+ */
+public class TestController {
+}
