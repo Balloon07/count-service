c86e8c5a92485baab7243dedef685c54c7b794c4
王思远 <1731748271@qq.com>
2024-02-21 18:04:40 +0800

    Jackson日志打印配置

--- count-app/src/main/java/com/balloon/common/util/JsonUtil.java
+++ count-app/src/main/java/com/balloon/common/util/JsonUtil.java
@@ -1,9 +1,16 @@
 package com.balloon.common.util;
 
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -12,7 +19,53 @@ import java.util.List;
  */
 public class JsonUtil {
 
-    private final static ObjectMapper objectMapper = new ObjectMapper();
+    private static final String PATTERN = "yyyy-MM-dd HH:mm:ss";
+    private static final ObjectMapper objectMapper;
+
+    static {
+        objectMapper = new ObjectMapper();
+        // 在ObjectMapper对象中配置JavaTimeModule，此为全局配置
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(Date.class, new DateJsonSerializer());
+        javaTimeModule.addDeserializer(Date.class, new DateJsonDeserializer());
+        objectMapper.registerModule(javaTimeModule);
+    }
+
+    /**
+     * Date序列化处理
+     */
+    public static class DateJsonSerializer extends JsonSerializer<Date> {
+
+        @Override
+        public void serialize(Date value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            if (value == null) {
+                gen.writeNull();
+                return;
+            }
+            SimpleDateFormat dateFormat = new SimpleDateFormat(PATTERN);
+            gen.writeString(dateFormat.format(value));
+        }
+    }
+
+    /**
+     * Date反序列化处理
+     */
+    public static class DateJsonDeserializer extends JsonDeserializer<Date> {
+
+        @Override
+        public Date deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+            String date = p.getText();
+            if (StringUtils.isBlank(date)) {
+                return null;
+            }
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat(PATTERN);
+            try {
+                return simpleDateFormat.parse(date);
+            } catch (ParseException e) {
+                throw new RuntimeException("日期解析失败，date=" + date);
+            }
+        }
+    }
 
     private JsonUtil() {
     }
